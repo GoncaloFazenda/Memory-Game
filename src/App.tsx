8c9a11e55316ba57fa8e6a5f7045863130f12efa
Gonçalo Fazenda <goncalo.fazenda@logirix.com>
2023-12-31 02:57:37 +0000

    Config: Shadcn UI config  & Page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-    return <h1 className="text-3xl font-bold underline bg-red-500">Tailwind is Working!</h1>;
+    return (
+        <div className="flex flex-1 bg-slate-500">
+            <div className="flex self-center justify-center w-full">
+                <h1 className="text-3xl font-bold underline bg-red-500">Tailwind is Working!</h1>
+                <Button>ShadCN is Working!</Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
