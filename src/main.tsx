ecca5e4a356cec676e5a1a5c2aba6029453ad2a4
Gonçalo Fazenda <goncalo.fazenda@logirix.com>
2024-01-01 02:03:42 +0000

    UI: Adding Themes

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from './_presentation/_components/theme-provider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+            <Router>
+                <App />
+            </Router>
+        </ThemeProvider>
     </React.StrictMode>
 );
